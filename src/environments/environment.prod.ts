a9d33be9f9a7161340c645ba2661dd02c2fcf55a
Eliie58 <elies.y@hotmail.com>
2022-05-11 11:49:18 +0200

    Build App
    Add Build Docs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  API_KEY: 'uzvVXrKLcVPauYJh6tQwpTjDutnnxkFD',
+  NETWORK: 'ropsten',
+  ADDRESS: 'https://eth-ropsten.alchemyapi.io/v2/uzvVXrKLcVPauYJh6tQwpTjDutnnxkFD',
+  CONTRACT_ADDRESS: "0x13F281480880a93A006a60D44acaF3Ed050aD6c9"
 };
