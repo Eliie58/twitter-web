55c92d7648e1e5ebf6f87abc6c4a25b6d2580b27
Eliie58 <elies.y@hotmail.com>
2022-05-11 18:19:33 +0200

    Update docs
    Update docs to update contract address

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   API_KEY: 'uzvVXrKLcVPauYJh6tQwpTjDutnnxkFD',
   NETWORK: 'ropsten',
   ADDRESS: 'https://eth-ropsten.alchemyapi.io/v2/uzvVXrKLcVPauYJh6tQwpTjDutnnxkFD',
-  CONTRACT_ADDRESS: "0x13F281480880a93A006a60D44acaF3Ed050aD6c9"
+  CONTRACT_ADDRESS: "0x2307Fe51bC5f347F3d4fCf117e3b5602340EbF2f"
 };
